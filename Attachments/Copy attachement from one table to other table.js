6206cd3ff56469c7fb9137588d39ec9d0b7ab3ab
Prashant Kumar <prashantmrshine@gmail.com>
2024-10-22 09:16:11 +0530

    Update Copy attachement from one table to other table.js

--- Attachments/Copy attachement from one table to other table.js
+++ Attachments/Copy attachement from one table to other table.js
@@ -1,11 +1,1 @@
-// Business Rule that runs on Update of specific field or record, currently its written on incident table whic can be replaced by any table
-(function executeRule(current, previous /*null when async*/ ) {
-
-    // Add your code here
-    var copy = new GlideRecord('problem');
-    copy.addQuery('parent', current.getUniqueValue());
-    copy.query();
-    if (copy.next()) {
-        GlideSysAttachment.copy('incident', current.getUniqueValue(), 'problem', copy.getUniqueValue()); // Here the attachment is copied from incident to problem, those table can be changed according to the needs
-    }
-})(current, previous);
+abdc
